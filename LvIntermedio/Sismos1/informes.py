51e37428ca21dea5c4cd983df109a31d811a5ca2
Nakrz <151976098+Nakrz@users.noreply.github.com>
2024-01-06 18:09:26 -0500

    intermedio1

--- LvIntermedio/Sismos1/informes.py
+++ LvIntermedio/Sismos1/informes.py
@@ -1,16 +1,16 @@
 
 def informeDeRiesgo(ciudades):
     for ciudad in ciudades:
-        sismos_ciudad = ciudad["sismos"]
-        if sismos_ciudad:
-            promedio = sum(sismos_ciudad) / len(sismos_ciudad)
-            if promedio < 2.5:
+        sismosPorCd = ciudad["sismos"]
+        if sismosPorCd:
+            ultimoSR = sismosPorCd[-1]
+            if ultimoSR < 2.5:
                 color = "Amarillo"
                 riesgo = "Sin riesgo"
-            elif 2.5 <= promedio <= 4.5:
+            elif 2.5 <= ultimoSR <= 4.5:
                 color = "Naranja"
                 riesgo = "Riesgo medio"
             else:
                 color = "Rojo"
                 riesgo = "Riesgo alto"
-            print(f"{ciudad['nombre']} [{promedio}] [{color}] [{riesgo}]")
+            print(f"{ciudad['nombre']} [{ultimoSR}] [{color}] [{riesgo}]")
